b4641ce086609af6190d03145df05d816346298d
Koichiro Iijima <koichiro.iijima.1979@gmail.com>
2021-05-04 20:45:20 +0900

    Update latest tag.

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ cd "$(dirname "$0")"
 BASE_IMAGE=bullseye-20210408-slim
 VERSION=${BASE_IMAGE}-20210504
 docker build .  -t debian_base:${VERSION} --build-arg IMAGE_VERSION=${VERSION} --build-arg IMAGE_NAME="debian_base" --build-arg BASE_IMAGE=${BASE_IMAGE}
-#docker tag debian_base:${VERSION} debian_base:latest
+docker tag debian_base:${VERSION} debian_base:latest
 
 docker login
 docker tag debian_base:${VERSION} koichiroiijima/debian_base:${VERSION}
-#docker tag debian_base:${VERSION} koichiroiijima/debian_base:latest
+docker tag debian_base:${VERSION} koichiroiijima/debian_base:latest
 docker push koichiroiijima/debian_base:${VERSION}
-#docker push koichiroiijima/debian_base:latest
+docker push koichiroiijima/debian_base:latest
