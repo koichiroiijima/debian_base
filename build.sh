405d3fa88fd9065d9fa340ed08dc7dcd3d090f6c
koichiroiijima <koichiro.iijima.1979@gmail.com>
2024-12-31 09:58:22 +0900

    Merge bookworm branch.

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ docker tag debian_base:${VERSION} debian_base:latest
 
 docker login
 docker tag debian_base:${VERSION} koichiroiijima/debian_base:${VERSION}
-#docker tag debian_base:${VERSION} koichiroiijima/debian_base:latest
+docker tag debian_base:${VERSION} koichiroiijima/debian_base:latest
 docker push koichiroiijima/debian_base:${VERSION}
-#docker push koichiroiijima/debian_base:latest
+docker push koichiroiijima/debian_base:latest
